c81fab55088e17ae0326f29568522900bf6ea367
Aaron D. Wickman <wickmanaaron@gmail.com>
2019-05-28 15:37:08 -0400

    completed sign up page and mobile responsive styles & navigation

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,14 @@ $(function(){
         $(".navigation").addClass('scroll');
         $("#blueLogo").removeClass("inactive");
         $("#logo").addClass("inactive");
+        $("#blueMenu").removeClass("inactive");
+        $("#menu").addClass("inactive");
     } else {
         $(".navigation").removeClass('scroll');
         $("#blueLogo").addClass("inactive");
         $("#logo").removeClass("inactive");
+        $("#blueMenu").addClass("inactive");
+        $("#menu").removeClass("inactive");
     }
     // Toggle Navbar Class by scrolling
     $(window).scroll(function() {
@@ -17,11 +21,22 @@ $(function(){
             $(".navigation").addClass('scroll');
             $("#blueLogo").removeClass("inactive");
             $("#logo").addClass("inactive");
+            $("#blueMenu").removeClass("inactive");
+            $("#menu").addClass("inactive");
         } else {
             $(".navigation").removeClass('scroll');
             $("#blueLogo").addClass("inactive");
             $("#logo").removeClass("inactive");
+            $("#blueMenu").addClass("inactive");
+            $("#menu").removeClass("inactive");
         }
+    });
+
+    $(".nav-menu").click(function() {
+        $('.side-menu').toggleClass("active");
+    });
+    $("#exit").click(function () {
+        $('.side-menu').toggleClass("active");
     });
 
 });
